012b46574a7d7db0d51075bd21e7e739ab71e8fd
Jonas <jonas.burba@gmail.com>
2024-08-02 16:27:22 +0300

    add dynamic fixed size add

--- src/app/components/Ads.tsx
+++ src/app/components/Ads.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
-
-import React, { useEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 import { ImagesQuery } from '../queries'
 
 const everyNthSlotAdPredicate = (
@@ -34,13 +33,26 @@ const cache = { current: new Array<string>() }
 export const useAds = (query: ImagesQuery) => {
   const ref = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     ref.current?.querySelectorAll('.ad-slot:empty').forEach(adSlot => {
       if (!cache.current.includes(adSlot.id)) {
         cache.current.push(adSlot.id)
+
         googletag.cmd.push(() => {
           const slot = googletag
-            .defineSlot('/6355419/Travel', ['fluid'], adSlot.id)!
+            .defineSlot(
+              '/6355419/Travel/Europe/France/Paris',
+              // if continous width/height was supported we could use the ad container square dimnesions to set ad dimensions on request.
+              // if discrete width/height was supported that cover multiple ad container square dimensions we could use the ad container square dimensions to set ad dimensions on request.
+              // ads would need to be cleared and added again on window resize both for fixed ad size and responsive ad size mode.
+              // ads do not get updated on window resize automatically because they are set on initial ad request.
+              // window resize watcher would need to be debounced because ads cannot be requested too frequently.
+              [
+                100 ?? document.getElementById(adSlot.id)?.clientWidth,
+                100 ?? document.getElementById(adSlot.id)?.clientHeight,
+              ],
+              adSlot.id,
+            )!
             .addService(googletag.pubads())
 
           googletag.display(slot)

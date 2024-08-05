f4f6225a7667f87cbeeb060cde25d3f5f2ef5c77
Jonas <jonas.burba@gmail.com>
2024-08-05 23:59:44 +0300

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* TODO: JB: check other strategies */}
       <head>
         <Script
           strategy="beforeInteractive"
